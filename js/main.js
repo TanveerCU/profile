76ad3e2649b4dec8c6b55e37565cb9d46c6e8624
tanveer <atanveer712@gmail.com>
2021-03-04 03:26:20 +0600

    new commit

--- js/main.js
+++ js/main.js
@@ -18,3 +18,57 @@
         }
     })
 })();
+
+
+
+/*--------------------------- portfolio filter and popup -------------------------*/
+(()=>{
+    const filterContainer = document.querySelector(".portfolio-filter"),
+    portfolioItemsContainer = document.querySelector(".portfolio-items"),
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    popup = document.querySelector(".portfolio-popup"),
+    prevBtn = popup.querySelector(".pp-prev"),
+    nextBtn = popup.querySelector(".pp-next"),
+    closeBtn = popup.querySelector(".pp-close"),
+    projectDetailsContainer = popup.querySelector(".pp-details"),
+    projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+    let itemIndex, slideIndex, screenshots;
+
+    // filter portfolio items
+    filterContainer.addEventListener('click',(event)=>{
+        if(event.target.classList.contains('filter-item') &&  !event.target.classList.contains("active")){
+            // deactivate existing active
+            filterContainer.querySelector(".active").classList.remove("outer-shadow","active");
+            // active new tab-item
+            event.target.classList.add("active","outer-shadow")
+            const target = event.target.getAttribute("data-target");
+            console.log(target);
+            portfolioItems.forEach((item,i)=>{
+                let catagory = item.getAttribute('data-catagory');
+                if( catagory === target || target === 'all'){
+                    // item.classList.add()
+                    item.classList.remove('hide');
+                    item.classList.add('show');
+                   console.log(portfolioItems[i],i);
+                } 
+                else{
+                    item.classList.add('hide');
+                    item.classList.remove('show');
+                }
+                // portfolioItems[i].classList.remove('hide');
+            })
+           
+        }
+        else{
+            console.log('false');
+        }
+    });
+
+    portfolioItemsContainer.addEventListener('click',(event)=>{
+        console.log(event);
+
+    });
+
+
+
+})();
